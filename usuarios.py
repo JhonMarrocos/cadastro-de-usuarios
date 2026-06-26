4bd3e6988129b82f3bec61c8a7e4f65f187dccce
Jonathas Marrocos <jhonmarrokin@hotmail.com>
2026-06-26 10:48:21 -0300

    commit v2

--- usuarios.py
+++ usuarios.py
@@ -1,181 +1,226 @@
 # Bibliotecas Necessarias
 
 from time import sleep as sl
-import hashlib
+import platform
 import os
-import json
 import stdiomask
+import hashlib
+import json
 
 # VariáVeis Principais
 
-usuarios = {"Emails": [],
-            "Usuarios": [],
-            "Senhas": []
-            }
+usuarios = [] # Armazena (Email, Usuario, Senha) na variavel usuarios
 
-caminho = r'C:\Users\Marrocos\Desktop\Repositorio CODE\cadastro de usuarios\cadastros.json' # Banco de dados dos cadastrados
+local_arquivo = os.path.dirname(os.path.abspath(__file__)) # Localiza o script do programa
+
+arquivo_json = os.path.join(local_arquivo, "cadastros.json") # Banco de dados dos cadastrados
 
 # Funções
 
 def limpar(): # Limpa o Terminal
-    os.system('cls')
 
-def criptografar_senha(senha): # Criptografa a senhas usando hash
-    return hashlib.sha256(senha.encode()).hexdigest()
+    if platform.system() == "Windows":
+        os.system("cls")
 
-def Progress(): # Barra de Loading similar ao usando no tqdm
-    
+    else:
+        os.system("clear")
+
+def pausar(): # Pausa o terminal
+    return input('ENTER para continuar...')
+
+def loading(): # Barra de Loading similar ao usando no tqdm
+
     for n in range(101):
         i = "■" * (n * 25 // 101)
         print(f"\r{n}% {i}", end="", flush=True)
         sl(0.01)
     print()
 
-def Titulo(text): # Retorta um Cabeçalho
+def titulo(text): # Retorta um Cabeçalho
+
     linhas = "=" * 30
     return f"{linhas}\n{text.center(len(linhas))}\n{linhas}"
 
+def encontrar_email(email): # Verifica se existe o email na lista de usuarios
+    for i, item in enumerate(usuarios):
+        if item["Email"] == email:
+            return i
+    return -1
+
+def encontrar_usuario(usuario): # Verifica se existe o usuario na lista de usuarios
+    for i, item in enumerate(usuarios):
+        if item["Usuario"] == usuario:
+            return i
+    return -1
+
+def criptografar_senha(senha): # Criptografa a senhas usando hash
+
+    return hashlib.sha256(senha.encode()).hexdigest()
+
 def carregar(): # Carrega os Dados dos usuarios cadastrados
+
     global usuarios
 
     try:
-        with open(caminho, "r", encoding="utf-8") as arq:
+        with open(arquivo_json, "r", encoding="utf-8") as arq:
             usuarios = json.load(arq)
 
     except FileNotFoundError:
         escrever()
 
 def escrever(): # Escreve/Atualiza dados dos usuarios cadastrados
-    with open(caminho, "w", encoding="utf-8") as arq:
-        json.dump(usuarios, arq, indent=4)
 
-def Main(): # Corpo Principal do Programa
+    with open(arquivo_json, "w", encoding="utf-8") as arq:
+        json.dump(usuarios, arq, indent=4, ensure_ascii=False)
 
+def main(): # Corpo Principal do Programa
     while True:
-        
-        print(Titulo("XYZ CORP"))
-        Progress()
-        print(Titulo("|1| - Cadastrar\n|2| - Remover\n|3| - Logar\n|4| - Usuarios\n|0| - Sair"))
-        
+        print(titulo("XYZ CORP"))
+        print("|1| - Cadastrar\n|2| - Remover\n|3| - Logar\n|4| - Usuarios\n|0| - Sair")
+
         try:
-            opc = int(input("Escolha Uma Opcao: "))
+            opc = int(input("\nEscolha Uma Opcao: "))
+            loading()
             limpar()
-            Progress()
+
             match opc:
 
                 case 1:
                     while True:
-                        cad_email = str(input("Digite Seu Email: ")).strip()
+                        email = str(input("Digite Seu Email: ")).strip()
+                        limpar()
 
-                        if not '@' in cad_email or not '.com' in cad_email:
+                        if not '@' in email or not '.com' in email:
                             print("Email inválido!")
+                            pausar()
+                            limpar()
                             continue
-                        
-                        if cad_email in usuarios["Emails"]:
-                            print(f"O Email {cad_email} ja Existe!")
+
+                        indice = encontrar_email(email)
+
+                        if indice != -1:
+                            print(f"O Email {email} ja Existe!")
+                            pausar()
+                            limpar()
                             continue
 
                         while True:
-                            cad_usuario = str(input("Digite um Nome de Usuario: ")).strip()
-                            
-                            if cad_usuario in usuarios["Usuarios"]:
-                                print(f"O Usuario {cad_usuario} ja esta Cadastrado!")
+                            usuario = str(input("Digite um Nome de Usuario: ")).strip()
+                            limpar()
+
+                            indice = encontrar_usuario(usuario)
+
+                            if indice != -1:
+                                print(f"O Usuario {usuario} ja esta Cadastrado!")
+                                pausar()
+                                limpar()
 
                             else:
                                 break
 
-                        cad_senha = str(input("Escolha uma Senha de no minimo 8 Caracteres: ")).strip()
+                        senha = str(input("Escolha uma Senha de no minimo 8 Caracteres: ")).strip()
+                        limpar()
 
-                        if len(cad_senha) < 8:
-                            print(f"A senha {cad_senha} e invalida!")
+                        if len(senha) < 8:
+                            print(f"A senha {senha} e invalida!")
+                            pausar()
+                            limpar()
                             continue
 
                         else:
-                            usuarios["Emails"].append(cad_email)
-                            usuarios["Usuarios"].append(cad_usuario)
-                            usuarios["Senhas"].append(criptografar_senha(cad_senha))
+                            usuarios.append({
+                                "Email": email,
+                                "Usuario": usuario,
+                                "Senha": criptografar_senha(senha)
+                            })
 
                             print("Cadastrando...")
-                            Progress()
-                            print("Cadastro Realizado com Sucesso!")
+                            loading()
+                            print("\nCadastro Realizado com Sucesso!")
                             escrever()
-                            input('Aperte ENTER para continuar...')
+                            pausar()
                             limpar()
 
                         break
-                        
+
                 case 2:
                     login = str(input("Usuario: ")).strip()
                     senha = stdiomask.getpass(prompt='Senha: ', mask='*')
+                    limpar()
 
-                    if login in usuarios["Usuarios"]:
-                        idx = usuarios["Usuarios"].index(login)
+                    idx = encontrar_usuario(login)
 
-                        if usuarios["Senhas"][idx] == criptografar_senha(senha):
-                        
-                            remover = input(
-                                "Tem certeza que deseja excluir a conta? S/N: "
-                            ).strip().upper()
+                    if idx != -1 and usuarios[idx]["Senha"] == criptografar_senha(senha):
 
-                            if remover == "S":
-                                usuarios["Emails"].pop(idx)
-                                usuarios["Usuarios"].pop(idx)
-                                usuarios["Senhas"].pop(idx)
+                        remover = input(
+                            "Tem certeza que deseja excluir a conta? ([S] || [N]): "
+                        ).strip().upper()
+                        limpar()
 
-                                escrever()
-                                print("Usuário removido com sucesso!")
+                        if remover == "S":
+                            usuarios.pop(idx)
+                            escrever()
+                            print("Usuário removido com sucesso!")
+                            pausar()
+                            limpar()
 
-                            elif remover == "N":
-                                print("Voltando ao menu...")
-
-                        else:
-                            print("Usuario ou Senha Invalida!")
+                        elif remover == "N":
+                            print("Voltando ao menu...")
+                            loading()
 
                     else:
                         print("Usuario ou Senha Invalida!")
-                    
-                    input('Aperte ENTER Para Continuar...')
-                    limpar()
-                    
+                        pausar()
+                        limpar()
+
                 case 3:
                     login = str(input("Usuario: ")).strip()
                     senha = stdiomask.getpass(prompt='Senha: ', mask='*')
+                    limpar()
 
-                    if login in usuarios["Usuarios"]:
-                        idx = usuarios["Usuarios"].index(login)
-                        if usuarios["Senhas"][idx] == criptografar_senha(senha):
-                            print(f"Bem Vindo {login}!")
-                        else:
-                            print("Usuario ou Senha Invalida!")
+                    idx = encontrar_usuario(login)
+
+                    if idx != -1 and usuarios[idx]["Senha"] == criptografar_senha(senha):
+                        print(f"Bem Vindo {login}!")
+                        pausar()
+                        limpar()
+                    
                     else:
                         print("Usuario ou Senha Invalida!")
-                    
-                    input('Aperte ENTER para continuar...')
-                    limpar()
+                        pausar()
+                        limpar()
+
                     continue
 
                 case 4:
-                    for i, email in enumerate(usuarios["Emails"]):
-                        print(f"|{i+1}| - Nick: {usuarios['Usuarios'][i]} Email: {email}")
-                    
+                    for i, item in enumerate(usuarios):
+                        print(f"|{i+1}| - Nick: {item['Usuario']} Email: {item['Email']}")
+
                     print()
-                    input("Aperte ENTER Para Continuar...")
+                    pausar()
                     limpar()
                     continue
 
                 case 0:
                     print("Saindo...")
-                    Progress()
                     print("Ate Mais!")
+                    sl(2)
                     break
 
                 case _:
                     print("Opcao Invalida!")
+                    pausar()
+                    limpar()
 
         except ValueError:
             print("Digite Somente Numeros!")
+            pausar()
+            limpar()
 
-# Main
-
+# main
+print('Iniciando...')
+loading()
+pausar()
+limpar()
 carregar()
-Main()
+main()
